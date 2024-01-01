890b5915728e1d833d4381b47ad77cc135f3ff8d
poswalsameer <poswalsameer2003@gmail.com>
2024-01-01 23:27:29 +0530

    completed the UI, only bg image is remaining, now moving onto the logic part

--- keyforge/src/App.js
+++ keyforge/src/App.js
@@ -4,9 +4,56 @@ function App() {
   return (
     <>
 
-      <h1 className='bg-red-500 text-black' >This is the setup of the project</h1>
+      {/* DIV FOR THE HEADING OF THE WEBSITE */}
+      <div className=' m-12 flex flex-row justify-center'>
 
-      <p className='text-6xl'>this is a paragraph</p>
+          <div className='text-8xl font-extrabold'>
+              KEY
+          </div>
+
+          <div className=' text-orange-400 text-8xl font-extrabold' >
+              FORGE
+          </div>
+
+      </div>
+
+      <div className='flex justify-center text-2xl text-blue-400 font-bold'>Your GO-TO Password Generator</div>
+      
+      <div className=' mt-32 flex flex-row justify-center' >
+
+        <input type="text" name="password" id="password" readOnly className=' h-10 w-2/6 rounded-lg border-4 border-orange-400 ' />
+
+        <button className=' mx-5 font-bold bg-blue-600 h-10 w-20 rounded-lg' >COPY</button>
+
+      </div>
+
+      
+          <div className='mt-20 flex flex-row justify-center'>
+
+              <label htmlFor="length" className='mx-6 font-extrabold text-xl text-orange-400 ' >Length of your password</label>
+              <input type="range" name="length" id="length" className='h-5 w-60 mx-6 mt-[6px]' />
+
+          </div>
+
+          <div className='flex flex-row justify-center' >
+
+              <div className=' m-10 mt-20 flex flex-row justify-center' >
+
+                <label htmlFor="numbers" className=' mx-6 font-extrabold text-xl text-orange-400' >NUMBERS?</label>
+                <input type="checkbox" name="numbers" className='h-6 w-6 mt-[3.5px] '/>
+
+              </div>
+
+              <div className=' m-10 mt-20 flex flex-row justify-center' >
+
+                <label htmlFor="numbers" className=' mx-6 font-extrabold text-xl text-orange-400' >CHARACTERS?</label>
+                <input type="checkbox" name="numbers" className='h-6 w-6 mt-[3.5px] border-2 border-blue-600'/>
+
+              </div>
+
+          </div>
+      
+      
     </>
   );
 }
