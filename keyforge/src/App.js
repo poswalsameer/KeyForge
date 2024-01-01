652ba8aec369fce6233e3e65653d4b8fce52a47f
poswalsameer <poswalsameer2003@gmail.com>
2024-01-01 22:52:23 +0530

    Setup of CRA and tailwind css

---
+++ keyforge/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+      <h1 className='bg-red-500 text-black' >This is the setup of the project</h1>
+
+      <p className='text-6xl'>this is a paragraph</p>
+    </>
+  );
+}
+
+export default App;
